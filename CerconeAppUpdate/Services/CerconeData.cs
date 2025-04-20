edb76d8f402756fe6e1543cfef8143c5a096a36c
Ulaspbit <ulaspbit@gmail.com>
2025-04-19 22:24:23 -0600

    Se agrego para descargar los datos del grimorio del google sheets

--- CerconeAppUpdate/Services/CerconeData.cs
+++ CerconeAppUpdate/Services/CerconeData.cs
@@ -1,5 +1,6 @@
 ﻿using CerconeClient.Dtos;
 using CerconeClient.Helpers;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,38 +8,69 @@ using System.Net.Http;
 using System.Reflection;
 using System.Text;
 using System.Text.Json;
+using System.Windows.Forms;
 
 namespace CerconeClient.Services
 {
-    public  class CerconeData
+    public class CerconeData
     {
         public void UpdatePsjData(string path)
         {
             using HttpClient client = new HttpClient();
-            HttpResponseMessage response = client.GetAsync("https://sheets.googleapis.com/v4/spreadsheets/1RY3WbHMMKznrKheDQYYrbnCIfk9ukOigpAR84cuUHIk/values/ROSTER?key=AIzaSyDbgaqwB_8Yt5FQdZdYg7_iLv2__1mmRtc").Result;
-            var data = response.Content.ReadAsStringAsync().Result;
-            var dto = JsonSerializer.Deserialize<GoogleSheetData>(data,new JsonSerializerOptions() { });
-            CreateFilesPsj(dto, path);
+            string[] sheets = new string[2]
+            { 
+            "https://sheets.googleapis.com/v4/spreadsheets/1RY3WbHMMKznrKheDQYYrbnCIfk9ukOigpAR84cuUHIk/values/ROSTER?key=AIzaSyDbgaqwB_8Yt5FQdZdYg7_iLv2__1mmRtc", 
+            "https://sheets.googleapis.com/v4/spreadsheets/1RY3WbHMMKznrKheDQYYrbnCIfk9ukOigpAR84cuUHIk/values/GRIMORIO?key=AIzaSyDbgaqwB_8Yt5FQdZdYg7_iLv2__1mmRtc" 
+            };
+
+            HttpResponseMessage response = client.GetAsync(sheets[0]).Result;
+            HttpResponseMessage grimoireResponse = client.GetAsync(sheets[1]).Result;
+
+            var rosterData = response.Content.ReadAsStringAsync().Result;
+            var grimoireData = grimoireResponse.Content.ReadAsStringAsync().Result;
+            var dto = JsonSerializer.Deserialize<GoogleSheetData>(rosterData, new JsonSerializerOptions() { });
+            var grimoireDto = JsonSerializer.Deserialize<GoogleSheetData>(grimoireData, new JsonSerializerOptions() { });
+            CreateFilesPsj(dto!, grimoireDto!, path);
         }
-        private void CreateFilesPsj(GoogleSheetData dto,string path)
+        private void CreateFilesPsj(GoogleSheetData dto, GoogleSheetData grimoireDto, string path)
         {
             var pjs = new List<PjsInfo>();
+            var grimoire = new List<GrimoireInfo>();
             dto.values.Remove(dto.values.First());
             dto.values.Remove(dto.values.First());
+            grimoireDto.values.Remove(grimoireDto.values.First());
+
             foreach (var rows in dto.values)
             {
-                if (rows.First() == "") continue;
-                var pj=PjHelper.GetPjInfo(rows);
-                if(pj!=null)pjs.Add(pj);
+                if (rows[0] == "") continue;
+                var pj = PjHelper.GetPjInfo(rows);
+                if (pj != null) pjs.Add(pj);
             }
-            BuildLuaData(pjs, path);
+
+            foreach (var rows in grimoireDto.values)
+            {   
+                if (rows == null || rows[0] == "") continue;
+                try{
+                    var grimoireInfo = GrimoireHelper.GetGrimoireInfo(rows);
+                    if (grimoireInfo != null) grimoire.Add(grimoireInfo);
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show($"Error al procesar la fila: {string.Join(", ", rows)}. Error: {e.Message}");
+                }
+            }
+
+            BuildLuaData(pjs, grimoire, path);
         }
-        private void BuildLuaData(List<PjsInfo> pjs,string path) 
+
+        private void BuildLuaData(List<PjsInfo> pjs, List<GrimoireInfo> grimoire, string path)
         {
-            var strindToSave=ConvertToLua(pjs);
+            var strindToSave = ConvertPjsToLua(pjs);
+            var strindToSave2 = ConvertGrimToLua(grimoire);
             File.WriteAllText(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty, @"CerconePjData.lua"), strindToSave);
+            File.WriteAllText(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty, @"CerconeGrimData.lua"), strindToSave2);
         }
-        private string ConvertToLua(List<PjsInfo> pjs)
+        private string ConvertPjsToLua(List<PjsInfo> pjs)
         {
             StringBuilder luaStringBuilder = new StringBuilder();
 
@@ -91,6 +123,28 @@ namespace CerconeClient.Services
                 luaStringBuilder.AppendLine($"            Percepcion={pj.HabilidadesNOCombatientes.Percepcion},");
                 luaStringBuilder.AppendLine($"            Fuerza={pj.HabilidadesNOCombatientes.Fuerza}");
                 luaStringBuilder.AppendLine("        }");
+                luaStringBuilder.AppendLine("    },");
+            }
+
+            luaStringBuilder.AppendLine("}");
+
+            return luaStringBuilder.ToString();
+        }
+
+        private static string ConvertGrimToLua(List<GrimoireInfo> grimoire)
+        {
+            StringBuilder luaStringBuilder = new StringBuilder();
+
+            luaStringBuilder.AppendLine("CerconeGrimoireData = {");
+            foreach (var data in grimoire)
+            {
+                luaStringBuilder.AppendLine($"    {{");
+                luaStringBuilder.AppendLine($"        Z=\"{data.Z?.Replace("\n", " ") ?? string.Empty}\",");
+                luaStringBuilder.AppendLine($"        Nombre=\"{data.NombreClase?.Replace("\n", " ") ?? string.Empty}\",");
+                luaStringBuilder.AppendLine($"        NombreRama=\"{data.NombreRama?.Replace("\n", " ") ?? string.Empty}\",");
+                luaStringBuilder.AppendLine($"        Orden=\"{data.Orden?.Replace("\n", " ") ?? string.Empty}\",");
+                luaStringBuilder.AppendLine($"        Descripcion=\"{data.Descripcion?.Replace("\n", "\\n") ?? string.Empty}\",");
+                luaStringBuilder.AppendLine($"        Valores=\"{data.Valores?.Replace("\n", " ") ?? string.Empty}\",");
                 luaStringBuilder.AppendLine("    },");
             }
 
